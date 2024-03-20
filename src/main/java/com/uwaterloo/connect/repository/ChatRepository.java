81316bf383ea3bbfddcb660381f74e41fc7e5c65
Sunvir <sunvirsingh72@gmail.com>
2024-03-20 18:25:42 -0400

    Fixed getChats API

--- src/main/java/com/uwaterloo/connect/repository/ChatRepository.java
+++ src/main/java/com/uwaterloo/connect/repository/ChatRepository.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 @Repository
 public interface ChatRepository extends JpaRepository<Chat, Integer> {
+
+    @Query("SELECT c FROM Chat c WHERE (c.fromUserId = ?1 and c.toUserId = ?2) OR (c.fromUserId = ?2 and c.toUserId = ?1)")
     List<Chat> findByFromUserIdAndToUserId(Integer fromUserId, Integer toUserId);
 
     @Query("SELECT DISTINCT c.toUserId FROM Chat c WHERE c.fromUserId = ?1")
